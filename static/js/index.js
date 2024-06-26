e3a91d10d6954961f12f4599afe58b9488513ced
kodai <kodai030513@gmail.com>
2024-06-26 11:34:22 +0900

    dom操作の修正

--- static/js/index.js
+++ static/js/index.js
@@ -111,11 +111,13 @@ const addProgressBar = () => {
 
 const updateProgressBar = (percent) => {
     const html = `
-        <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: ${percent}%">
-            ${percent}%
+        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-6 mt-6">
+            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: ${percent}%">
+                ${percent}%
+            </div>
         </div>`;
     
-    document.getElementById("progress-bar").innerHTML = html;
+    document.getElementById("rakuten-spaces-progress-bar").innerHTML = html;
 }
 
 
